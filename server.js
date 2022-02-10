b83ece413c8d988c43b69a8bcfb27e1d3e273a67
pegasus1982 <gdm19920108@gmail.com>
2022-02-10 11:00:22 +0800

    fix: found some errors and fix them

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ function respondHello (req, res) {
 function respondUserAgent (req, res) {
   const ua = req.headers['user-agent']
   res.end(JSON.stringify({ ua }))
+}
 function respondBase64 (req, res) {
   const phrase = req.url.replace(/^\/b64\//, '')
   res.end(JSON.stringify({ b64: Buffer.from(phrase).toString('base64') }))
@@ -25,4 +26,4 @@ function respondBase64 (req, res) {
 server.listen(PORT)
 console.log(`Server listening on port ${PORT}`)
 
-if (require.main !== module) module.exports = server
+if (require.main !== module) module.exports = server;
